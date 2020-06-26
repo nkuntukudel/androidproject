c0379b238c068174c6738e0ba5523e0c5aceb37a
Naveen Bhat <bhatk1981@gmail.com>
2020-06-26 15:12:54 +0530

    Signup task

---
+++ NPS16SIGNUP/app/src/main/java/com/qq/nps16signup/activity_final.java
@@ -0,0 +1,14 @@
+package com.qq.nps16signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_final extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final2);
+    }
+}
